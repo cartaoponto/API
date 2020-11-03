9fe334bdf5d8f06c27e90689fdc6f38700930217
nicolaspaulor <nicolaspaulo.r@gmail.com>
2020-11-03 20:32:09 -0300

    Gerando projeto spring com spring initializr.

---
+++ cartaoponto/src/main/java/br/com/cartaoponto/api/CartaopontoApplication.java
@@ -0,0 +1,13 @@
+package br.com.cartaoponto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartaopontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartaopontoApplication.class, args);
+	}
+
+}
---
+++ cartaoponto/src/test/java/br/com/cartaoponto/api/CartaopontoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cartaoponto.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartaopontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
